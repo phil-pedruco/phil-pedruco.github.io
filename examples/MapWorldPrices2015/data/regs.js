e0757b22439ee15952b4e2c93f61fd76f4aba1c9
philip pedruco <philip.pedruco@gmail.com>
2015-04-20 08:23:40 +1000

    world prices 2015 inital commit

---
+++ examples/MapWorldPrices2015/data/regs.js
@@ -0,0 +1,234 @@
+var regs = [
+    {
+        "key": "Argentina",
+        "regex": "Argentina"
+    },
+    {
+        "key": "Australia",
+        "regex": "Australia|Melbourne|Sydney"
+    },
+    {
+        "key": "Austria",
+        "regex": "Austria"
+    },
+    {
+        "key": "Belgium",
+        "regex": "Belgium"
+    },
+    {
+        "key": "Brazil",
+        "regex": "Brazil|Rio de Janeiro|Sao Paulo"
+    },
+    {
+        "key": "Canada",
+        "regex": "Canada|Ottawa|Toronto"
+    },
+    {
+        "key": "Chile",
+        "regex": "Chile"
+    },
+    {
+        "key": "China",
+        "regex": "China|Beijing|Shanghai"
+    },
+    {
+        "key": "Colombia",
+        "regex": "Colombia"
+    },
+    {
+        "key": "CostaRica",
+        "regex": "CostaRica"
+    },
+    {
+        "key": "Czech Republic",
+        "regex": "Czech Republic"
+    },
+    {
+        "key": "Denmark",
+        "regex": "Denmark"
+    },
+    {
+        "key": "Egypt",
+        "regex": "Egypt"
+    },
+    {
+        "key": "Estonia",
+        "regex": "Estonia"
+    },
+    {
+        "key": "Euro area",
+        "regex": "Euro area"
+    },
+    {
+        "key": "Finland",
+        "regex": "Finland"
+    },
+    {
+        "key": "France",
+        "regex": "France|Paris"
+    },
+    {
+        "key": "Germany",
+        "regex": "Germany|Berlin|Frankfurt"
+    },
+    {
+        "key": "Greece",
+        "regex": "Greece"
+    },
+    {
+        "key": "Hong Kong",
+        "regex": "Hong Kong"
+    },
+    {
+        "key": "Hungary",
+        "regex": "Hungary"
+    },
+    {
+        "key": "India",
+        "regex": "India|Bangalore|Mumbai|NewDelhi"
+    },
+    {
+        "key": "Indonesia",
+        "regex": "Indonesia|Jakarta"
+    },
+    {
+        "key": "Ireland",
+        "regex": "Ireland"
+    },
+    {
+        "key": "Israel",
+        "regex": "Israel"
+    },
+    {
+        "key": "Italy",
+        "regex": "Italy"
+    },
+    {
+        "key": "Japan",
+        "regex": "Japan|Tokyo"
+    },
+    {
+        "key": "Latvia",
+        "regex": "Latvia"
+    },
+    {
+        "key": "Lithuania",
+        "regex": "Lithuania"
+    },
+    {
+        "key": "Malaysia",
+        "regex": "Malaysia|Kuala Lumpur"
+    },
+    {
+        "key": "Mexico",
+        "regex": "Mexico|Mexico City"
+    },
+    {
+        "key": "Netherlands",
+        "regex": "Netherlands"
+    },
+    {
+        "key": "New Zealand",
+        "regex": "New Zealand|Auckland|Wellington"
+    },
+    {
+        "key": "Norway",
+        "regex": "Norway"
+    },
+    {
+        "key": "Pakistan",
+        "regex": "Pakistan"
+    },
+    {
+        "key": "Peru",
+        "regex": "Peru"
+    },
+    {
+        "key": "Philippines",
+        "regex": "Philippines|Manila"
+    },
+    {
+        "key": "Poland",
+        "regex": "Poland"
+    },
+    {
+        "key": "Portugal",
+        "regex": "Portugal"
+    },
+    {
+        "key": "Russia",
+        "regex": "Russia|Moscow"
+    },
+    {
+        "key": "Saudi Arabia",
+        "regex": "Saudi Arabia"
+    },
+    {
+        "key": "Singapore",
+        "regex": "Singapore"
+    },
+    {
+        "key": "South Africa",
+        "regex": "South Africa|Cape Town|Johannesburg"
+    },
+    {
+        "key": "South Korea",
+        "regex": "South Korea"
+    },
+    {
+        "key": "Spain",
+        "regex": "Spain"
+    },
+    {
+        "key": "Sri Lanka",
+        "regex": "Sri Lanka"
+    },
+    {
+        "key": "Sweden",
+        "regex": "Sweden"
+    },
+    {
+        "key": "Switzerland",
+        "regex": "Switzerland|Zurich"
+    },
+    {
+        "key": "Taiwan",
+        "regex": "Taiwan"
+    },
+    {
+        "key": "Thailand",
+        "regex": "Thailand"
+    },
+    {
+        "key": "Turkey",
+        "regex": "Turkey"
+    },
+    {
+        "key": "UAE",
+        "regex": "UAE"
+    },
+    {
+        "key": "Ukraine",
+        "regex": "Ukraine"
+    },
+    {
+        "key": "United Kingdom",
+        "regex": "United Kingdom|Edinburgh|London|Britain"
+    },
+    {
+        "key": "United States",
+        "regex": "United States|Boston|Chicago|New York City|San Francisco"
+    },
+    {
+        "key": "Uruguay",
+        "regex": "Uruguay"
+    },
+    {
+        "key": "Venezuela",
+        "regex": "Venezuela"
+    },
+    {
+        "key": "Vietnam",
+        "regex": "Vietnam"
+    }
+];
